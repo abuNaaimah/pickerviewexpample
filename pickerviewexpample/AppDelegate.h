3acdd2b640af186ffc8f35a00181f2cf44196558
tops <tops@topss-iMac-3.local>
2016-07-26 13:17:07 +0530

    Initial Commit

---
+++ pickerviewexpample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pickerviewexpample
+//
+//  Created by tops on 7/26/16.
+//  Copyright (c) 2016 tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pickerviewexpample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pickerviewexpample
+//
+//  Created by tops on 7/26/16.
+//  Copyright (c) 2016 tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
